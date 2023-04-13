9e48d82cd7498acedd6b5bf3259355f95095af19
gretaTomasoni <greta51198@gmail.com>
2023-04-13 12:34:39 +0200

    Aggiunto footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,28 @@ import styles from "./footer.module.scss";
 const Footer = () => {
   return (
     <div className={styles.Footer}>
-      <h3>FOOTER</h3>
+      <div className={styles.upper}>
+        <div>
+          <h3 className={styles.logo}>LOGO</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing.</p>
+        </div>
+        <ul className={styles.link}>
+          <li>
+            <a href="#">Twitter</a>
+          </li>
+          <li>
+            <a href="#">Youtube</a>
+          </li>
+          <li>
+            <a href="#">Instagram</a>
+          </li>
+        </ul>
+      </div>
+      <hr className={styles.line} />
+      <div className={styles.down}>
+        <p>Privacy Policy</p>
+        <p>EventHub 2023</p>
+      </div>
     </div>
   );
 };
