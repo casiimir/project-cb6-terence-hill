c2310e1db75f8d18a82c9deea5c13a211f2c833e
FabioFarinella96 <fabiofarinella96@gmail.com>
2023-04-14 12:08:38 +0200

    aggiunta homepage desktop

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,6 +5,16 @@ const Navbar = () => {
   return (
     <div className={styles.Navbar}>
       <h4>LOGO</h4>
+      <ul className={styles.nav}>
+        <li className={styles.navItem}>Festival</li>
+        <li className={styles.navItem}>Musica</li>
+        <li className={styles.navItem}>Arte & Teatro</li>
+        <li className={styles.navItem}>Sport</li>
+      </ul>
+      <div className={styles.search__section}>
+        <span className={styles.loginBtn}>login</span>
+        <span className={styles.searchBtn}>Search</span>
+      </div>
       <div className={styles.hamburgerMenu}>
         <div className={styles.line}></div>
         <div className={styles.line}></div>
--- src/components/textHero/TextHero.jsx
+++ src/components/textHero/TextHero.jsx
@@ -1,9 +1,23 @@
 import styles from "./index.module.scss";
 import CtaButton from "../ctaButton";
+import Image from "next/image";
 
 const TextHero = () => {
   return (
     <div className={styles.TextHero}>
+      <Image
+        className={styles.image}
+        src="https://images.unsplash.com/photo-1565035010268-a3816f98589a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80"
+        width={1000}
+        height={300}
+        alt="hero image"
+        priority
+      />
+      <h1 className={styles.desktopTitle}>
+        Una miriade di <br /> eventi imperdibili
+      </h1>
+
+      {/* mobile  */}
       <h1 className={styles.title}>
         Trova eventi imperdibili <br /> nella tua città
       </h1>
@@ -12,7 +26,9 @@ const TextHero = () => {
         quaerat adipisci officia amet dolorum corporis explicabo officiis, culpa
         recusandae!
       </p>
-      <CtaButton text={"COMPRA I BIGLIETTI"} />
+      <div className={styles.btn}>
+        <CtaButton text={"COMPRA I BIGLIETTI"} />
+      </div>
     </div>
   );
 };
