251f69bd58446f5294ee192d64c318371daf4fa2
MariMessi <marilenamessina62@gmail.com>
2023-04-20 16:26:46 +0200

    fixing modal message

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 import React from "react";
 import styles from "./modal.module.scss";
 
-const Modal = ({ isOpen, onClose }) => {
+const Modal = ({ isOpen, onClose, modalText }) => {
   if (!isOpen) return null;
 
   return (
     <div className={styles.Modal}>
       <div onClick={onClose} className={styles.overlay}></div>
       <div className={styles.modalContent}>
-        <h1 className={styles.modalH1}>Complimenti per il tuo acquisto!</h1>
+        <h1 className={styles.modalH1}>{ modalText }</h1>
         <hr className={styles.line} />
         {/* <p className={styles.modalP}>
           Siamo impazienti di incontrarti al tuo evento.
