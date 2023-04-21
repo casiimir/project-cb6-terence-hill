d35555958422dbda0ea6421bd1af04152cfe5c87
StefRapis <stefaniarapisarda.itaca@gmail.com>
2023-04-21 17:34:34 +0200

    arricchiamo il footer!

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,6 +4,23 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className={styles.Footer}>
+      <div className={styles.newsletter}>
+        <div className={styles.news_testo}>
+          <h4 className={styles.titolo}>Iscriviti alla Newsletter</h4>
+          <p>
+            Rimani aggiornato sulle ultime novitá e non perderti nessun evento.
+          </p>
+        </div>
+        <div className={styles.news_input}>
+          <input
+            className={styles.input_field}
+            type="text"
+            placeholder="Inserisci la tua email"
+          />
+          <input className={styles.input_btn} type="submit" value="Iscriviti" />
+        </div>
+      </div>
+
       <div className={styles.upper}>
         <div className={styles.left__section}>
           <Image
@@ -15,18 +32,74 @@ const Footer = () => {
             priority
           />
           <p>La tua destinazione per i migliori eventi.</p>
+
+          <p>qualcosaaaaaaaaaaa</p>
         </div>
-        <ul className={styles.link}>
-          <li>
-            <a href="#">Twitter</a>
-          </li>
-          <li>
-            <a href="#">Youtube</a>
-          </li>
-          <li>
-            <a href="#">Instagram</a>
-          </li>
-        </ul>
+
+        <div className={styles.all_links}>
+          <div className={styles.titoli_footer}>
+            <ul className={styles.link}>
+              <li>
+                <p className={styles.titoletto}>Assistenza Clienti</p>
+              </li>
+              <li>
+                <a href="#">Condizioni di vendita </a>
+              </li>
+              <li>
+                <a href="#">Diritto di recesso</a>
+              </li>
+              <li>
+                <a href="#">Rivendita biglietti</a>
+              </li>
+              <li>
+                <a href="#">Metodi di consegna</a>
+              </li>
+              <li>
+                <a href="#">Metodi di pagamento</a>
+              </li>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles.titoli_footer}>
+            <ul className={styles.link}>
+              <li>
+                <p className={styles.titoletto}>Guide EventHub</p>
+              </li>
+              <li>
+                <a href="#">Festival Finder </a>
+              </li>
+              <li>
+                <a href="#">Guida eventi sportivi</a>
+              </li>
+              <li>
+                <a href="#">Guida teatro</a>
+              </li>
+              <li>
+                <a href="#">PopEvents</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles.titoli_footer}>
+            <ul className={styles.link}>
+              <li>
+                <p className={styles.titoletto}>Social</p>
+              </li>
+              <li>
+                <a href="#">Twitter</a>
+              </li>
+              <li>
+                <a href="#">Youtube</a>
+              </li>
+              <li>
+                <a href="#">Instagram</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
       <hr className={styles.line} />
       <div className={styles.down}>
