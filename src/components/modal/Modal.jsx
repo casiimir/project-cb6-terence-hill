cf765f2998153e23ed705a2efa7fb1d85e6be96b
FabioFarinella96 <fabiofarinella96@gmail.com>
2023-04-23 12:03:39 +0200

    added login page

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,30 +1,30 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import React from "react";
 import styles from "./modal.module.scss";
 
 const Modal = ({ isOpen, onClose, modalText }) => {
   if (!isOpen) return null;
-  const router = useRouter();
+  // const router = useRouter();
 
   return (
     <div className={styles.Modal}>
       <div onClick={onClose} className={styles.overlay}></div>
       <div className={styles.modalContent}>
-        <h1 className={styles.modalH1}>{ modalText }</h1>
+        <h1 className={styles.modalH1}>{modalText}</h1>
         <hr className={styles.line} />
         {/* <p className={styles.modalP}>
           Siamo impazienti di incontrarti al tuo evento.
         </p> */}
 
         <div className={styles.modalLink}>
-        <Link href="/" className={styles.modalP}>
-          Torna alla home
-        </Link>
-        <Link href="#" onClick={() => router.back()} className={styles.modalP}>
+          <Link href="/" className={styles.modalP}>
+            Torna alla home
+          </Link>
+          {/* <Link href="#" onClick={() => router.back()} className={styles.modalP}>
           Torna alla pagina precedente
-        </Link>
-      </div>
+        </Link> */}
+        </div>
       </div>
     </div>
   );
